228c0573d6cc15998ddb9c366d047e8b8c981710
Natan Reduzino <natanfreduzino@gmail.com>
2026-06-26 17:31:27 -0300

    painel admin

--- motorartigos/views.py
+++ motorartigos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404
 from django.core.paginator import Paginator
 from .models import Artigo, EixoTecnologia
 from django.contrib import messages
